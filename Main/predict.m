465dbb8bac4b9fa70d90937cb363058b0a5fe3b9
Toby Tianxiao Zhao <tzh@kth.se>
2016-12-11 20:45:57 +0100

    Update predict.m

--- Main/predict.m
+++ Main/predict.m
@@ -9,7 +9,11 @@
 %           mu_bar(t)         3X1
 %           sigma_bar(t)      3X3
 function [mu_bar,sigma_bar] = predict(mu,sigma,u,R)
+% mu_bar = g(u(t), mu(t-1)) = mu(t-1) + u(t)
 mu_bar = mu + u;
+
+% G(1,3) = -u(2) = -v*delta_t*sin(theta)
+% G(2,3) = u(1) = v*delta_t*cos(theta)
 G = [1 0 -u(2); 0 1 u(1); 0 0 1];
 sigma_bar = G * sigma * G' + R;
 

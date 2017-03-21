0321bb1975d687a8ac69ac5db0cc5126dee31de5
Tianxiao Zhao <tzh@kth.se>
2017-03-21 16:05:27 +0100

    Update associate.m

--- Main/associate.m
+++ Main/associate.m
@@ -13,7 +13,7 @@
 %           nu^i(t)             2XN
 %           S^i(t)              2X2XN
 %           H^i(t)              2X3XN
-function [c,outlier, nu, S, H] = associate(mu_bar,sigma_bar,z_i,M,Lambda_m,Q)
+function [c, outlier, nu, S, H] = associate(mu_bar, sigma_bar, z_i, M, Lambda_m, Q)
 z_hat = [];
 nu = [];
 S = [];
@@ -21,17 +21,17 @@ H = [];
 P = [];
 
 for j = 1 : size(M, 2)
-  z_hat(:,j) = observation_model(mu_bar, M, j);
-  H(:,:,j) = jacobian_observation_model(mu_bar, M, j, z_hat(:,j), 1);
-  S(:,:,j) = H(:,:,j) * sigma_bar * H(:,:,j)' + Q;
-  nu(:,j) = z_i - z_hat(:,j);
-  nu(2,j) = mod(nu(2,j) + pi, 2 * pi) - pi;
-  P(j) = 1/sqrt(det(2 * pi * S(:,:,j))) * exp((-1/2) * nu(:,j)' * inv(S(:,:,j)) * nu(:,j));
+  z_hat(:, j) = observation_model(mu_bar, M, j);
+  H(:, :, j) = jacobian_observation_model(mu_bar, M, j, z_hat(:, j), 1);
+  S(:, :, j) = H(:, :, j)*sigma_bar*H(:, :, j)' + Q;
+  nu(:, j) = z_i - z_hat(:, j);
+  nu(2, j) = mod(nu(2, j) + pi, 2*pi) - pi;
+  P(j) = 1/sqrt(det(2*pi*S(:, :, j))) * exp((-1/2) * nu(:, j)' * inv(S(:, :, j)) * nu(:, j));
 end
 
 [P_max c] = max(P);
 
-DM = nu(:,c)' * inv(S(:,:,c)) * nu(:,c);
+DM = nu(:, c)' * inv(S(:, :, c)) * nu(:, c);
 if DM > Lambda_m
   outlier = 1;
 else

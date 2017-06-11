47eb591320a29fde404dffa640f63a61d61bb8e4
Tianxiao Zhao <tzh@kth.se>
2017-06-12 00:06:45 +0200

    Create observation_model.m

--- Main/observation_model.m
+++ Main/observation_model.m
@@ -7,7 +7,7 @@
 %           j           1X1
 % Outputs:  
 %           h           2X1
-function h = observation_model(x,M,j)
+function h = observation_model(x, M, j)
 h11 = sqrt((M(1,j) - x(1))^2 + (M(2,j) - x(2))^2);
 h21 = atan2(M(2,j) - x(2), M(1,j) - x(1)) - x(3);
 h21 = mod(h21 + pi, 2 * pi) - pi;
